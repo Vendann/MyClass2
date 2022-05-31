d93d7e751d7391f5cfe8bfd9425fbb2c67a655a3
Vendann <tigrun324@gmail.com>
2022-05-31 18:10:35 +0300

    Добавьте файлы проекта.

---
+++ Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    abstract class Item
+    {
+        protected long invNumber; // инвентарный номер – целое число
+        protected bool taken; // хранит состояние объекта – имеется в библиотеке или нет, то есть взят на руки
+
+        public bool IsAvailable() // истина, если предмет имеется в библиотеке
+        {
+            if (taken == true) return true; else return false;
+        }
+        public long GetInvNumber() // инвентарный номер
+        {
+            return invNumber;
+        }
+        public void Take() // операция "взять"
+        {
+            taken = false;
+        }
+        abstract public void Return(); // операция "вернуть"
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Состояние единицы хранения:\n Инвентарный номер: {0}\n Наличие: {1}", invNumber, taken);
+        }
+
+        public Item(long invNumber, bool taken)
+        {
+            this.invNumber = invNumber;
+            this.taken = taken;
+        }
+
+        public Item()
+        {
+            this.taken = true;
+        }
+
+        public void TakeItem()
+        {
+            if (this.IsAvailable()) this.Take();
+        }
+    }
+}
